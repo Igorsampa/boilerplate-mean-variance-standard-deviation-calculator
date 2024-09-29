5df534cf3f597b38a82c51821baa45b655c92286
Igorsampa <igor.silva@sou.inteli.br.edu>
2024-09-29 13:11:40 -0300

    feat:anwser

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+  
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # Converte a lista em uma matriz 3x3 usando Numpy
+    matrix = np.array(list).reshape(3, 3)
+    
+    # Calcula os valores ao longo dos eixos 0 e 1
+    calculations = {
+        'mean': [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()],
+        'variance': [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()],
+        'standard deviation': [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()],
+        'max': [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()],
+        'min': [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()],
+        'sum': [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()]
+    }
 
     return calculations
